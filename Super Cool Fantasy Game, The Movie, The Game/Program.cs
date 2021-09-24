a32686b0a7178076466edab2bbeec4aeb54ae7c0
KyleDixonVO <KyleDixon@hotmail.ca>
2021-09-24 09:14:46 -0300

    Initial Upload

---
+++ Super Cool Fantasy Game, The Movie, The Game/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Cool_Fantasy_Game__The_Movie__The_Game
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            
+            float badGuyXP;
+            badGuyXP = 10.0f;
+            float koboldXP = badGuyXP / 2.0f;
+            float goblinXP = badGuyXP * 2.0f;
+            float orcXP = badGuyXP * 3.5f;
+            float lichXP = badGuyXP * 10.0f;
+            float playerHealth;
+            playerHealth = 100.0f;
+            int lives;
+            lives = 1;
+            int spellSlots;
+            spellSlots = 5;
+            int enemiesKilled;
+            float swordDamage;
+            swordDamage = 10.0f;
+            float orcHealth;
+            orcHealth = 100.0f;
+            string attack = "attack";
+            string block = "block";
+            string orc;
+            string userInput = null;
+            float experience;
+            bool doAttack = attack.Equals(userInput);
+          
+            experience = 0.0f;
+
+            Core();
+
+            void inputManager()
+            {
+
+                
+            }
+
+            void Core()
+            {
+                Console.WriteLine("An orc attacks!");
+                Console.WriteLine("Orc Health: " + orcHealth);
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("attack / block / cast / run");
+                Console.WriteLine("(I only have attack working at the moment)");
+                userInput = Console.ReadLine();
+                doAttack = attack.Equals(userInput);
+                if (doAttack == true)
+                { 
+                    orcHealth = orcHealth - swordDamage;
+                    Console.WriteLine("Orc Health: " + orcHealth);
+                }
+                
+                if ()
+                Console.ReadKey(true);
+            }
+            
+        }
+
+      
+
+        
+
+    }
+
+
+
+    
+
+}
+
+        
+
+   
+    
+
